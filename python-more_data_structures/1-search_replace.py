f76238b59c4c9f92a5b6d82188b9889842a88491
Aysel06 <mammedovaaysel0@gmail.com>
2026-03-29 19:04:40 +0000

    1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
- def search_replace(my_list, search, replace):
+def search_replace(my_list, search, replace):
     new_list = []
+
     for i in my_list:
         if i==search:
             new_list.append(replace)
