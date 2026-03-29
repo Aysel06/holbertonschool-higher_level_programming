256dcb17d65cda3c097b809330218fea63d4e488
Aysel06 <mammedovaaysel0@gmail.com>
2026-03-29 19:15:56 +0000

    1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-
-    for i in my_list:
-        if i==search:
+    for num in my_list:
+        if num == search:
             new_list.append(replace)
         else:
-            new_list.append(i)
+            new_list.append(num)
     return new_list
