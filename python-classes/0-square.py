0491941222fe31e9c07af4bef397b5256276ff39
Aysel06 <mammedovaaysel0@gmail.com>
2026-04-05 19:38:56 +0000

    0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-""" This module defines a Square class """
+"""This module defines a Square class"""
 class Square:
-    """ An emtpy class that defines a square """
+    """An empty class that defines a square"""
     pass
-if _name_ == "__main__":
-    my_square = Square()
-    print(type(my_square))
-    print(my_square._dict_)
